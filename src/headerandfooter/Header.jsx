c3be3543c0f0b413ce977e2f4161485ad06e059e
Mandip Tamang <136143099+mandiptmg@users.noreply.github.com>
2023-12-28 22:53:49 +0545

    Update Header.jsx

--- src/headerandfooter/Header.jsx
+++ src/headerandfooter/Header.jsx
@@ -21,12 +21,9 @@ const Header = () => {
         <div   onClick={() => {
                   setClose(false)
                   setOpen(false)
-                  if (close === true) {
-                    document.documentElement.classList.remove('dark')
-                    localStorage.setItem('dark-mode', true)
-                  } else {
+                  if (close === false) {
                     document.documentElement.classList.add('dark')
-                    localStorage.setItem('dark-mode', false)
+                    localStorage.setItem('dark-mode', true)
                   }
                 }} >
           <NavLink to='/'>
